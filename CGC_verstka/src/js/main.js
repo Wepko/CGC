46e1867caff028d84680e7d685e418097a495a43
unknown <wepko.pro@mail.ru>
2021-09-11 16:43:33 +0300

    accordion

--- CGC_verstka/src/js/main.js
+++ CGC_verstka/src/js/main.js
@@ -1,17 +1,39 @@
-var animateButton = function(e) {
+import { Swiper } from 'swiper';
+import BadgerAccordion from 'badger-accordion';
 
-  e.preventDefault;
-  //reset animation
-  e.target.classList.remove('animate');
+{
   
-  e.target.classList.add('animate');
-  setTimeout(function(){
+}
+
+{
+  //const swiperOne = new Swiper('.slider', {loop:false})
+  
+// Creating a new instance of the accordion
+const accordionDomNode = document.querySelector('.js-badger-accordion');
+
+const accordion = new BadgerAccordion(accordionDomNode);
+  window.accordion = accordion;
+
+}
+
+
+
+{
+  var animateButton = function(e) {
+  
+    e.preventDefault;
+    //reset animation
     e.target.classList.remove('animate');
-  },700);
-};
-
-var bubblyButtons = document.getElementsByClassName("bubbly-button");
-
-for (var i = 0; i < bubblyButtons.length; i++) {
-  bubblyButtons[i].addEventListener('click', animateButton, false);
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
 }
