05bec97ac38c1c39a1be15766a8ab620a7295046
unknown <wepko.pro@mail.ru>
2021-09-13 06:18:31 +0300

    icone and othe

--- CGC_verstka/src/js/main.js
+++ CGC_verstka/src/js/main.js
@@ -2,38 +2,25 @@ import { Swiper } from 'swiper';
 import BadgerAccordion from 'badger-accordion';
 
 {
-  
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: false,
+    slidesPerView: 1,
+    centeredSlides: true,
+    spaceBetween: 30,
+
+  });
+
 }
 
 {
   //const swiperOne = new Swiper('.slider', {loop:false})
-  
-// Creating a new instance of the accordion
-const accordionDomNode = document.querySelector('.js-badger-accordion');
+    
+  // Creating a new instance of the accordion
+  const accordionDomNode = document.querySelector('.js-badger-accordion');
 
-const accordion = new BadgerAccordion(accordionDomNode);
+  const accordion = new BadgerAccordion(accordionDomNode);
   window.accordion = accordion;
 
-}
-
-
-
-{
-  var animateButton = function(e) {
-  
-    e.preventDefault;
-    //reset animation
-    e.target.classList.remove('animate');
-    
-    e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
 }
