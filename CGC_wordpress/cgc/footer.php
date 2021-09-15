5f95667502a85fdd3dc858911c44ba28d638f08d
unknown <wepko.pro@mail.ru>
2021-09-15 18:55:19 +0300

    wordpress_theme

---
+++ CGC_wordpress/cgc/footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package CGC
+ */
+
+?>
+
+
+
+
+<footer class="footer">
+		footer	
+	</footer>
+	<script src="js/main.min.js"></script>
+</body>
+<?php wp_footer(); ?>
+
+</html>
