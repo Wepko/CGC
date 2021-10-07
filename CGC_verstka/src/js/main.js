a2957b0631df80a538f6c3f9b65dc68e36b587bf
wepko <wepko.pro@mail.ru>
2021-10-07 10:40:58 +0300

    ready adaptive card-product

--- CGC_verstka/src/js/main.js
+++ CGC_verstka/src/js/main.js
@@ -28,11 +28,22 @@ import BadgerAccordion from 'badger-accordion';
   //   control.classList.toggle('o-play-btn--playing');
   // })
 
+  const cardProducts = [...document.querySelectorAll('.card-product')];
+
+  for (let cardProduct of cardProducts) {
+    cardProduct.addEventListener('pointerdown', () => {
+      cardProduct.classList.toggle('card-product-js');
+      console.log('41233')
+    });
+  }
+  
 
 }
 
 
 {
+
+  //кнопка еще при вскрытие
   const objsHidden = document.querySelectorAll('.projects__filter-buttons');
   const objHidden = [...objsHidden][1];
   const objToggle = document.querySelector('.projects__filter-next');
