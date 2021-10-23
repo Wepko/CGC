a8f675be8b3b81a6aee95bd3a4c081a4daa6e790
unknown <wepko.pro@mail.ru>
2021-10-23 10:07:27 +0300

    rebuild filter

--- CGC_verstka/src/js/main.js
+++ CGC_verstka/src/js/main.js
@@ -9,24 +9,6 @@ import BadgerAccordion from 'badger-accordion';
   const accordion = new BadgerAccordion(accordionDomNode);
   window.accordion = accordion;
 
-  // const accordions = document.querySelectorAll('.js-badger-accordion');
-  // const arr = []; 
-  // Array.from(accordions).forEach((accordion, index) => {
-
-  //     const ba = new BadgerAccordion(accordion);
-
-  //     // console.log(ba.getState([0]));
-  // });
-
- 
-}
-
-{
-
-  // const control = document.querySelector('.o-play-btn');
-  // control.addEventListener('click', (e) => {
-  //   control.classList.toggle('o-play-btn--playing');
-  // })
 
   const cardProducts = [...document.querySelectorAll('.card-product')];
 
@@ -36,35 +18,8 @@ import BadgerAccordion from 'badger-accordion';
     });
   }
   
-
 }
 
 
-{
-
-  //кнопка еще при вскрытие
-  const objsHidden = document.querySelectorAll('.projects__filter-buttons');
-  const objHidden = [...objsHidden][1];
-  const objToggle = document.querySelector('.projects__filter-next');
-
-  objToggle.addEventListener('click', () => {
-
-    const span = objToggle.children[0];
-    
-    const nameClassArr = objHidden.className.split(' ');
-    span.innerText = 'Еще';
-
-    if (nameClassArr.includes('hidden-js')) {
-      span.innerText = 'Скрыть';
- 
-      console.log(span.nextSibling);
-    }
-
-    
-    span.nextSibling.classList.toggle('anim-arrow-js');
-    objHidden.classList.toggle('hidden-js')
-  })
-  console.log(objHidden);
-}
 
 
