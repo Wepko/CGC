62015bd0c78b8aea606c729cbfcc2ce6a4bf25a6
unknown <wepko.pro@mail.ru>
2021-11-23 15:00:52 +0300

    slidere correct

--- template-parts/question.php
+++ template-parts/question.php
@@ -1,11 +1,11 @@
 <div class="questions">
-      <div class="container">
-        <div class="questions__wrapper">
-          <div class="questions__title">Остались 
-            <div class="text-accent">вопросы?</div>
-          </div>
-          <div class="questions__description">Оставьте свой номер телефона и мы перезвоним вам в ближайшее время</div>
-          <div class="questions__buttons"><a class="btn-primary" data-custom-open="modal-1" href="#">Заказать звонок</a></div>
-        </div>
-      </div>
-    </div>
+		<div class="container">
+			<div class="questions__wrapper">
+				<div class="questions__title">Остались 
+					<div class="text-accent">вопросы?</div>
+				</div>
+				<div class="questions__description">Оставьте свой номер телефона и мы перезвоним вам в ближайшее время</div>
+				<div class="questions__buttons"><a class="btn-primary" data-custom-open="modal-1" href="#">Заказать звонок</a></div>
+			</div>
+		</div>
+	</div>
