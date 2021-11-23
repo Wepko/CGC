5f103cc9de6a03df67429e706dc12126213f8d43
unknown <wepko.pro@mail.ru>
2021-11-23 19:03:01 +0300

    moda;

---
+++ template-parts/modal.php
@@ -0,0 +1,19 @@
+<div class="modal micromodal-slide" id="modal-1" aria-hidden="true">
+  <div class="modal__overlay" tabindex="-1" data-custom-close="modal-1">
+    <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
+      <header class="modal__header">
+        <h2 class="modal__title" id="modal-1-title">
+					Заказать проект
+        </h2>
+        <button class="modal__close" aria-label="Close modal" data-custom-close="modal-1"></button>
+      </header>
+      <main class="modal__content" id="modal-1-content">
+        <p>
+				Оставь заявку и мы перезвоним в течении 15 минут
+        </p>
+				<?php echo do_shortcode( '[contact-form-7 id="441" title="Контактная форма 1"]' ); ?>
+      </main>
+
+    </div>
+  </div>
+</div>
--- template-parts/question.php
+++ template-parts/question.php
@@ -1,11 +1,13 @@
+<?php get_template_part( 'template-parts/modal' ); ?>
+
 <div class="questions">
-		<div class="container">
-			<div class="questions__wrapper">
-				<div class="questions__title">Остались 
-					<div class="text-accent">вопросы?</div>
-				</div>
-				<div class="questions__description">Оставьте свой номер телефона и мы перезвоним вам в ближайшее время</div>
-				<div class="questions__buttons"><a class="btn-primary" data-custom-open="modal-1" href="#">Заказать звонок</a></div>
+	<div class="container">
+		<div class="questions__wrapper">
+			<div class="questions__title">Остались 
+				<div class="text-accent">вопросы?</div>
 			</div>
+			<div class="questions__description">Оставьте свой номер телефона и мы перезвоним вам в ближайшее время</div>
+			<div class="questions__buttons"><a class="btn-primary" data-custom-open="modal-1">Заказать звонок</a></div>
 		</div>
 	</div>
+</div>
