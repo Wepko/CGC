2a247d7f9f78277e7305ac1b299528e07a0b7172
unknown <wepko.pro@mail.ru>
2021-12-14 16:39:47 +0300

    dinamyc title modal

--- src/assets/js/components/modal.js
+++ src/assets/js/components/modal.js
@@ -2,7 +2,11 @@ import MicroModal from 'micromodal';
 
 
 MicroModal.init({
-	onShow: modal => console.info(`${modal.id} is shown`), // [1]
+	onShow: (modal, b, e) => {
+		const text = e.target.text;
+		const title =	modal.querySelector('#modal-1-title');
+		title.innerText = text;
+	}, // [1]
 	onClose: modal => console.info(`${modal.id} is hidden`), // [2]
 	openTrigger: 'data-custom-open', // [3]
 	closeTrigger: 'data-custom-close', // [4]
