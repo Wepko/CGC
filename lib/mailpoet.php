41da2eac4280cd235e1400e76928b5b780822bbd
unknown <wepko.pro@mail.ru>
2022-02-15 15:55:07 +0300

    mailpoet test12

--- lib/mailpoet.php
+++ lib/mailpoet.php
@@ -5,7 +5,10 @@ add_filter('mailpoet_newsletter_shortcode', 'mailpoet_custom_shortcode', 10, 6);
 function mailpoet_custom_shortcode($shortcode, $newsletter, $subscriber, $queue, $newsletter_body, $arguments) {
   // always return the shortcode if it doesn't match your own!
   if ($shortcode !== '[custom:table]') return $shortcode; 
-  
+	echo "newsletter";
+  pritnr($newsletter);
+	echo "newsletter";
+	
   $table =  "<table border=1>";
   $table .= "<thead><tr><th>Fruit Name</th><th>Fruit Color</th></tr></thead>";
   $table .= "<tbody><tr><td>Apple</td><td>Red</td></tr></tbody>";
