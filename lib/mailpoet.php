cfe75693d00985449548c8ab21f76e4c786e9e3f
unknown <wepko.pro@mail.ru>
2022-02-15 15:40:15 +0300

    mailpoet test5

--- lib/mailpoet.php
+++ lib/mailpoet.php
@@ -1,12 +1,5 @@
 <?php 
-add_filter('mailpoet_newsletter_shortcode', 'mailpoet_custom_shortcode', 10, 6);
 
-function mailpoet_custom_shortcode($shortcode, $newsletter, $subscriber, $queue, $newsletter_body, $arguments) {
-  // always return the shortcode if it doesn't match your own!
-
-  
-  return 'asdf';
-}
 
 add_filter('mailpoet_newsletter_shortcode_link', 'mailpoet_custom_shortcode_referral_link', 10, 5);
 
@@ -18,4 +11,16 @@ function mailpoet_custom_shortcode_referral_link($shortcode, $newsletter, $subsc
   $referral_link = "http://example.com/?referral={$referral}";
   
   return $referral_link;
+}
+
+
+add_filter('mailpoet_newsletter_shortcode', 'mailpoet_custom_shortcode', 10, 6);
+
+function mailpoet_custom_shortcode($shortcode, $newsletter, $subscriber, $queue, $newsletter_body, $arguments) {
+  // always return the shortcode if it doesn't match your own!
+  if ($shortcode !== '[custom:tool]') return $shortcode; 
+  
+  $table =  "<p style='background:red;'>tool</p>";
+  
+  return $table;
 }
