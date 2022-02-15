3c3ca25e668e69b4802ef6b29e871d51be35c056
unknown <wepko.pro@mail.ru>
2022-02-15 15:49:14 +0300

    mailpoet test10

--- lib/mailpoet.php
+++ lib/mailpoet.php
@@ -1,18 +1,5 @@
 <?php 
 
-
-add_filter('mailpoet_newsletter_shortcode_link', 'mailpoet_custom_shortcode_referral_link', 10, 5);
-
-function mailpoet_custom_shortcode_referral_link($shortcode, $newsletter, $subscriber, $queue, $arguments) {
-  // always return the shortcode if it doesn't match your own!
-  if ($shortcode !== '[link:referral]') return $shortcode;
-  
-  $referral = 'MailPoet';
-  $referral_link = "http://example.com/?referral={$referral}";
-  
-  return $referral_link;
-}
-
 add_filter('mailpoet_newsletter_shortcode', 'mailpoet_custom_shortcode', 10, 6);
 
 function mailpoet_custom_shortcode($shortcode, $newsletter, $subscriber, $queue, $newsletter_body, $arguments) {
@@ -27,4 +14,8 @@ function mailpoet_custom_shortcode($shortcode, $newsletter, $subscriber, $queue,
   
   return $table;
 }
+
+
+
+	
 	
