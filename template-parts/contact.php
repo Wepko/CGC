0d588c3e81e9b88ef5a48df0f3402debafa9a5c3
unknown <wepko.pro@mail.ru>
2022-03-07 17:29:18 +0300

    update

--- template-parts/contact.php
+++ template-parts/contact.php
@@ -7,7 +7,10 @@
 				<div class="contacts__info-content">
 					<p class="icon-email"><?php echo get_field("contact-email", 117)?></p>
 					<p class="icon-phone"><?php echo get_field("contact-phone", 117)?></p>
-					<p class="icon-adress"><?php echo get_field("contact-adress", 117)?> <br> <?php echo get_field("contact-adress_2", 117)?></p>
+					<p class="icon-adress"><?php echo get_field("contact-adress", 117)?></p>
+					<?php if (!empty(get_field("contact-adress_2", 117))) : ?>
+						<p class="icon-adress"><?php echo get_field("contact-adress_2", 117)?></p>
+					<?php endif; ?>
 				</div>
 				<div class="contacts__info-social">
 					<div class="social">
