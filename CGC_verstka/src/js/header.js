399d68db49cc4f6f25fcaa12e65c376c602d39de
unknown <wepko.pro@mail.ru>
2022-04-28 18:48:02 +0300

    update

--- CGC_verstka/src/js/header.js
+++ CGC_verstka/src/js/header.js
@@ -1,25 +1,25 @@
 'use strict';
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
     
-//     ///// humburger
-//     const html = document.querySelector('html');
-//     const humburger = html.querySelector('.menu-toggle');
-//     const menu = html.querySelector('.navigation');
+    ///// humburger
+    const html = document.querySelector('html');
+    const humburger = html.querySelector('.menu-toggle');
+    const menu = html.querySelector('.header__wrapper .navigation');
 
-//     const links = menu.querySelectorAll('.navigation__subnav');
+    const links = menu.querySelectorAll('.navigation__subnav');
 
-//     for (let link of links) {
-//         link.addEventListener('click', () => {
-//             link.classList.toggle('active');
-//         });
-//     }
+    for (let link of links) {
+        link.addEventListener('click', () => {
+            link.classList.toggle('active');
+        });
+    }
 
-//     humburger.addEventListener('click', () => {
-//         humburger.classList.toggle('active');
-//         menu.classList.toggle('active');
-//         html.classList.toggle('active')
-//     })
+    humburger.addEventListener('click', () => {
+        humburger.classList.toggle('active');
+        menu.classList.toggle('active');
+        html.classList.toggle('active')
+    })
 
-// })
+})
 
